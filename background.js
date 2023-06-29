6d932d746c847cfcf6c507b160b8962912e899c4
Jagdish Patil <patil.jagdish222@gmail.com>
2023-06-29 18:02:25 +0530

    added fixed new tab issue

--- background.js
+++ background.js
@@ -1,13 +1,17 @@
-chrome.alarms.onAlarm.addListener(function (alarm) {
-  if (alarm) {
-    let url = chrome.runtime.getURL("./assets/jal-lijiye.mp3");
+// Create a variable to store the tab ID
+let tabId = null;
 
-    // set this string dynamically in your code, this is just an example
-    // this will play success.wav at half the volume and close the popup after a second
+// Create the new tab once
+chrome.tabs.create({}, function (newTab) {
+  tabId = newTab.id;
+});
+
+// Update the tab whenever the alarm fires
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  if (alarm && tabId !== null) {
+    let url = chrome.runtime.getURL("./assets/jal-lijiye.mp3");
     url += "?volume=0.5&src=jal-lijiye.mp3&length=1000";
 
-    chrome.tabs.create({
-      url,
-    });
+    chrome.tabs.update(tabId, { url: url });
   }
 });
